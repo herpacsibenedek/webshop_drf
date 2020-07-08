672a539bfaf80511311c61cfc0316e9080c974aa
Benedek Herpacsi <herpacsi.benedek@gmail.com>
2020-07-08 11:44:51 +0200

    CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Other informations', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    empty_value_display = 'N/A'
+    readonly_fields = ('last_login', 'date_joined')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/managers.py
+++ accounts/managers.py
@@ -15,15 +15,18 @@ class CustomUserManager(BaseUserManager):
 
         if not email:
             raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
         user.set_password(password)
         user.save()
         return user
 
     def create_superuser(self, email, password, **extra_fields):
         """
-        Create and save SuperUSer with a given email and password.
+        Create and save SuperUSer with the given email and password.
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
